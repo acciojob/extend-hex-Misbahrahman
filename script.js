81ecaec89942f183ad75ddba822d281e4c8a9a8a
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-19 17:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  let str = shortHex.slice(-3);
+  let res = "";
+  for(int i = 0 ; i < 3 ; i++){
+	  for(int j = 0 ; j < 3 ; j++){
+         res += str.charAt(i);	
+	  }
+  }	
+
+	return res;
+	
+	  
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
